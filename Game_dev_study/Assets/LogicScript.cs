ced52cb5e7e2ea83d85684babf7080f482473fa7
Arthur-ca <ch243852@gmail.com>
2023-04-23 02:21:56 -0300

    First game build - Angry Bird Fly

---
+++ Game_dev_study/Assets/LogicScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increment Score")]
+    public void incrementScore(int scoretoAdd)
+    {
+        playerScore = playerScore + scoretoAdd;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
